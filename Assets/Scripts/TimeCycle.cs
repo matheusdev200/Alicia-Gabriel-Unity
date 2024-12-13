89da846e3e52fefdf1e6d23b1bb0a77bcc7309e5
matheusdev200 <62211090+matheusdev200@users.noreply.github.com>
2024-12-13 16:57:39 -0300

    Aula 13-12 parte 2
    Sistema de dia e noite

--- Assets/Scripts/TimeCycle.cs
+++ Assets/Scripts/TimeCycle.cs
@@ -10,20 +10,68 @@ public class TimeCycle : MonoBehaviour
     public float dayIntensity = 3f;
     public float nightIntensity = 0.6f;
     [Header("Duração Dia/Noite")]
-    public float dayDuration;
+    public float dayDuration; //todos os tempos são em segundos
     public float nightDuration;
-    bool day = true;
+    private bool isDay = true;
+    [Header("Curvas")]
+    public AnimationCurve dayCurve;
+    public AnimationCurve nightCurve;
 
     //Acessar a luz principal -> Pegar a variável de Intensity -> animar a variável de "dia" pra "noite"
-    //Upgrade: realizar o mesmo processo na luz secundária
+    //Upgrade: realizar o mesmo processo na luz secundária,
+    //alterar um pouco as cores das luzes conforme anoitece/amanhece
 
     void Start()
     {
         //inicializa como dia -> e começa a ficar de noite -> volta a ser dia -> repete
+        StartCoroutine(DayCycle());
     }
-    void Update()
+    IEnumerator DayCycle()
     {
-
+        float currentTime = 0f;
+        while (isDay)//enquanto dia for true
+        {
+            //toda porcentagem -> é uma fração
+            //toda fração (em porcentagem) é a parte / todo
+            currentTime += Time.deltaTime;
+            mainLight.intensity = Mathf.Lerp(dayIntensity, nightIntensity,
+                dayCurve.Evaluate(currentTime / dayDuration));
+            //mainLight.color = Color.Lerp();
+            //secondaryLight.intensity = Mathf.Lerp(dayIntensity, nightIntensity, currentTime / dayDuration);
+            //interpolação linear
+            if (currentTime >= dayDuration)//se passou todo o tempo do dia, acabou o dia
+            {
+                isDay = false;
+                yield return null;
+            }
+            yield return null;
+        }
+        if (isDay == false)
+        {
+            StartCoroutine(NightCycle());
+        }
     }
-    
+    IEnumerator NightCycle()
+    {
+        float currentTime = 0f;
+        while (!isDay)//enquanto dia for false
+        {
+            //toda porcentagem -> é uma fração
+            //toda fração (em porcentagem) é a parte / todo
+            currentTime += Time.deltaTime;
+            mainLight.intensity = Mathf.Lerp(nightIntensity, dayIntensity,
+                nightCurve.Evaluate(currentTime / nightDuration));
+            //interpolação linear
+            if (currentTime >= nightDuration)//se passou todo o tempo do dia, acabou o dia
+            {
+                isDay = true;
+                yield return null;
+            }
+            yield return null;
+        }
+        if (isDay == true)
+        {
+            StartCoroutine(DayCycle());
+        }
+    }
 }
